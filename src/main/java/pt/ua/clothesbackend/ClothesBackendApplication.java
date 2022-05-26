cd4647ce93d6e16e24733bd2b79638c4774b3f47
komortokozak <komorowski.adam2000@gmail.com>
2022-05-26 14:59:29 +0100

    init

---
+++ src/main/java/pt/ua/clothesbackend/ClothesBackendApplication.java
@@ -0,0 +1,13 @@
+package pt.ua.clothesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pt/ua/clothesbackend/ClothesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ua.clothesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
